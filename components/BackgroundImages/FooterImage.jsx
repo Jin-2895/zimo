01ffa1943071509160728e1817e8906604c12b77
Naveed Javed <37734603+Jin-2895@users.noreply.github.com>
2023-03-17 01:11:43 +0500

    zimo

---
+++ components/BackgroundImages/FooterImage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterImage = () => {
+  return (
+    <div
+        className="bg-[url('/Images/global1.png')] h-screen w-screen" >
+    </div>
+  )
+}
+
+export default FooterImage
